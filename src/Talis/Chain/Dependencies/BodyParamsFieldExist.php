1d7ba0b96f1e9a8e7173ac1738d7c4f63dea0534
itay-moav <itay.malimovka@gmail.com>
2019-01-10 10:41:00 -0500

    modify and add dependencies for body

--- src/Talis/Chain/Dependencies/BodyParamsFieldExist.php
+++ src/Talis/Chain/Dependencies/BodyParamsFieldExist.php
@@ -1,11 +1,11 @@
 <?php namespace Talis\Chain\Dependencies;
 
 /**
- * Making sure that a body with some field was sent
+ * Making sure that a body with params was sent
  * 
  * @author Itay Moav
  */
-class BodyFieldExist extends aDependency{
+class BodyParamsFieldExist extends aDependency{
 	/**
 	 * 
 	 * {@inheritDoc}
@@ -14,13 +14,15 @@ class BodyFieldExist extends aDependency{
 	protected function validate():bool{
 		$body  = $this->Request->getBody();
 		$field = $this->params['field'];
-		return isset($body->$field);
+		$r = isset($body->params);
+		$b = isset($body->params->$field);
+		return $r&&$b;
 	}
 	
 	public function render(\Talis\commons\iEmitter $emitter):void{
 		\dbgr('RENDER input body',print_r($this->Request->getBody(),true));
 		$response = new \Talis\Message\Response;
-		$response->setMessage("Mising field [{$this->params['field']}] in request body");
+		$response->setMessage("Mising param [{$this->params['field']}] part in request body->params[]");
 		$response->markDependency();
 		$response->setStatus(new \Talis\Message\Status\Code500);
 		$emitter->emit($response);
