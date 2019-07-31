73f3e8551f2ab2e890e5ef1197a6f9cc75ae59ac
itay-moav <itay.malimovka@gmail.com>
2019-07-31 15:54:01 -0400

    utf8mb4 connection going forward

--- src/Talis/Services/Sql/MySqlClient.php
+++ src/Talis/Services/Sql/MySqlClient.php
@@ -131,7 +131,7 @@ class MySqlClient {
 		$dns = 'mysql:dbname=' . $conf_data ['database'] . ";host=" . $conf_data ['host'] . $p;
 		
 		$this->NativeDB = new \PDO ( $dns, $conf_data ['username'], $conf_data ['password'], [ 
-				\PDO::MYSQL_ATTR_INIT_COMMAND 		=> 'SET NAMES utf8',
+				\PDO::MYSQL_ATTR_INIT_COMMAND 		=> 'SET NAMES utf8mb4',
 				\PDO::ATTR_ERRMODE			  		=> \PDO::ERRMODE_EXCEPTION,
 				\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
 		] );
