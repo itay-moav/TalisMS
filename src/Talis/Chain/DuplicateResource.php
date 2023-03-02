9865f667f8e6ac2c147de9cc834331a0731dbd2b
Itay <itay.malimovka@gmail.com>
2023-03-02 18:03:56 -0500

    a duplicate resource message

---
+++ src/Talis/Chain/DuplicateResource.php
@@ -0,0 +1,34 @@
+<?php namespace Talis\Chain;
+
+/**
+ * Responsebility:
+ * 
+ *  Alerts the client that a create call is trying to create a duplicate resource, let's see how client can handle it
+ *  
+ * @author Itay Moav
+ * @Date  2023-03-02
+ */
+class DuplicateResource extends aChainLink implements \Talis\commons\iRenderable{
+    /**
+     * {@inheritDoc}
+     * @see \Talis\Chain\aChainLink::process()
+     */
+	public function process():aChainLink{
+		$this->Response->setMessage('duplicate resource');
+		$this->Response->setStatus(new \Talis\Message\Status\Code422);
+		$this->Response->markError();
+		return $this;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \Talis\commons\iRenderable::render()
+	 */
+	public function render(\Talis\commons\iEmitter $emitter):void{
+	    \ZimLogger\MainZim::$CurrentLogger->debug($this->Request->getUri() . ' CHAIN ENDS ABROPTLY: Trying to create a duplicate resource');
+	    \ZimLogger\MainZim::$CurrentLogger->debug('RESPONSE: ');
+	    \ZimLogger\MainZim::$CurrentLogger->debug($this->Response);
+		$emitter->emit($this->Response);
+	}
+}
